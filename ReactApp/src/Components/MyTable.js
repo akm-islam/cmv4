c531274c4c3d31dc45673f4fac1167f8f144c279
AKM Zahirul Islam <mamunjahir.com@gmail.com>
2022-10-01 16:45:55 -0400

    ra

--- ReactApp/src/Components/MyTable.js
+++ ReactApp/src/Components/MyTable.js
@@ -5,17 +5,17 @@ import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Popover from '@mui/material/Popover';
+import { CSVLink, CSVDownload } from 'react-csv';
+import Grid from '@mui/material/Grid';
 
 export default function BasicTable(props) {
   return (
-<Popover id={"id"} open={props.table_open} onClose={()=>props.Set_table_open(false)}>
-    <div style={{'backgroundColor':"white"}}>
+    <Popover id={"id"} open={props.table_open} onClose={() => props.Set_table_open(false)}>
+      <Grid container direction="row" justifyContent="space-around" >
+        <Grid item xs={9} sx={{paddingLeft:0,marginLeft:-3}}><p style={{margin:0,padding:0,fontSize:20,fontWeight:600}}>Tweets</p></Grid>
+        <Grid item><CSVLink data={props.selected_group_data} >Export</CSVLink></Grid>
+      </Grid>
       <Table sx={{ maxWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Tweets</TableCell>
-          </TableRow>
-        </TableHead>
         <TableBody>
           {props.selected_group_data.map((row) => (
             <TableRow key={row.raw_tweet} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
@@ -26,7 +26,6 @@ export default function BasicTable(props) {
           ))}
         </TableBody>
       </Table>
-      </div>
     </Popover>
   );
 }
